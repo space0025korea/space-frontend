7a3ebbf2a21fb264c83ed79c4d5c6b9c9812cab1
mynameisjisoo <devjisoolee@gmail.com>
2023-07-21 12:22:35 +0900

    graphql api fetch, 클라우디너리 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  async rewrites() {
+    return [{ source: "/graphql", destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql` }];
+  },
 };
 
 module.exports = nextConfig;
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,19 +1,54 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import Image from "next/image";
 
-import { getContent } from "pages/api/portfolio";
+import { GET_PROJECTS } from "pages/api/portfolio";
 
-const Portfolio = ({ contents }: { contents: any }) => {
+type DataType = {
+  attributes: {
+    area: string;
+    category: string;
+    description: string;
+    drawing: {
+      data: { id: number; attributes: { name: string; url: string } }[];
+    };
+    location: string;
+    photos: {
+      data: { id: number; attributes: { name: string; url: string } }[];
+    };
+    title: string;
+    updatedAt: string;
+  };
+  id: string;
+};
+
+const Portfolio = () => {
+  const { data, loading } = useQuery(GET_PROJECTS);
+  const projects: DataType[] = data?.projects?.data;
+
+  if (loading) return <h1>loading</h1>;
+  //TODO: loadingSpinner
+
   return (
     <>
-      <h1>dd</h1>
-      {contents.data.map(({ attributes, id }: { attributes: any; id: string }) => (
-        <li key={id} className="border-b-8 border-yellow-300 bg-yellow-50">
+      {projects.map(({ attributes, id }) => (
+        <li key={id} className="= border-b-8 border-yellow-300">
           <h1>{attributes.title}</h1>
           <p>{attributes.description}</p>
+          {attributes.photos.data.map((img) => (
+            <Image
+              src={img.attributes.url}
+              // TODO: 이미지 반응형으로 수정
+              width={200}
+              height={200}
+              alt="drawing images"
+              key={img.id}
+            />
+          ))}
         </li>
       ))}
     </>
   );
 };
-export const getServerSideProps = async () => getContent();
+
 export default Portfolio;
