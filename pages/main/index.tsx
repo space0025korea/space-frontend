24e4b6e45fa2843ca0c306251f51f0589f99a99f
mynameisjisoo <devjisoolee@gmail.com>
2023-08-11 18:11:15 +0900

    메인 이미지 동적으로 변경

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
 
 import FullPageLayout from "components/layout/full";
+import FullpageSpinenr from "components/spinner/fullpage";
+import Layout from "components/layout";
+import { GET_MAIN_IMAGE } from "pages/api/mainImage";
 
 const Main = () => {
-  return (
-    <FullPageLayout>
-      <div></div>
-    </FullPageLayout>
-  );
+  const { data, loading } = useQuery(GET_MAIN_IMAGE);
+
+  if (loading) {
+    return (
+      <Layout>
+        <FullpageSpinenr />
+      </Layout>
+    );
+  }
+  if (data) {
+    return (
+      <FullPageLayout
+        imgUrl={data.mainImage.data.attributes.image.data.attributes.url}
+      ></FullPageLayout>
+    );
+  } else {
+    return <FullPageLayout imgUrl={"/images/main1.jpeg"}></FullPageLayout>;
+  }
 };
 
 export default Main;
