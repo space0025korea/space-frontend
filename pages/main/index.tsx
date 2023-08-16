9699b31cfbffe8dde1ee26f093ee6801ac30a987
mynameisjisoo <devjisoolee@gmail.com>
2023-08-16 10:28:57 +0900

    메인 이미지 url 로 변경

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,30 +1,11 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
 
 import FullPageLayout from "components/layout/full";
-import FullpageSpinenr from "components/spinner/fullpage";
-import Layout from "components/layout";
-import { GET_MAIN_IMAGE } from "pages/api/mainImage";
 
 const Main = () => {
-  const { data, loading } = useQuery(GET_MAIN_IMAGE);
-
-  if (loading) {
-    return (
-      <Layout>
-        <FullpageSpinenr />
-      </Layout>
-    );
-  }
-  if (data) {
-    return (
-      <FullPageLayout
-        imgUrl={data.mainImage.data.attributes.image.data.attributes.url}
-      ></FullPageLayout>
-    );
-  } else {
-    return <FullPageLayout imgUrl={"/images/main1.jpeg"}></FullPageLayout>;
-  }
+  return (
+    <FullPageLayout imgUrl="https://res.cloudinary.com/design329/image/upload/main/main.jpg" />
+  );
 };
 
 export default Main;
