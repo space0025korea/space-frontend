93c87af8833dc972ec9a57b2b39ac1eba502d3ac
mynameisjisoo <devjisoolee@gmail.com>
2023-08-28 10:03:53 +0900

    footer 정보 수정, metatag desc 임시 문구 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,14 +5,14 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
-        {/* TODO: keyword, desc 작성 */}
-        <meta name="keyword" content="인테리어디자인,인테리어,329Design,329디자인" />
+        {/* TODO: keyword, desc 수정 */}
+        <meta name="keyword" content="인테리어 디자인,인테리어,329Design,329디자인" />
         <meta name="description" content="" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://329design.co.kr" />
         <meta property="og:title" content="329Design" />
         <meta property="og:image" content="https://329design.co.kr/images/logo/preview.png" />
-        <meta property="og:description" content="" />
+        <meta property="og:description" content="329Design" />
         <meta property="og:site_name" content="329Design" />
         <meta property="og:locale" content="ko_KR" />
         <meta
