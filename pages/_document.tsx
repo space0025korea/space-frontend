53b88cfa7ff2b5e51fc218dbb5a83acbc82f3b89
mynameisjisoo <devjisoolee@gmail.com>
2023-11-22 22:22:10 +0900

    메타태그 및 파비콘 파일 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,19 +9,22 @@ export default function Document() {
           name="keyword"
           content="인테리어 디자인, 인테리어, 공간제작소, 서울인테리어, 송파인테리어, 강남인테리어"
         />
-        <meta name="description" content="더 나은 공간을 고민하고 연구합니다." />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://329design.co.kr" />
-        <meta property="og:title" content="329디자인 | 329Design" />
-        <meta property="og:image" content="https://329design.co.kr/images/logo/preview.png" />
-        <meta property="og:description" content="더 나은 공간을 고민하고 연구합니다." />
-        <meta property="og:site_name" content="329디자인 | 329Design" />
-        <meta property="og:locale" content="ko_KR" />
-        <meta name="naver-site-verification" content="a503038f047e43414cd7480296ef2a397381003d" />
         <meta
-          name="google-site-verification"
-          content="9L4pCPdg-SVewotr4vo_iishXAFU9GssbVI4ic3MmCI"
+          name="description"
+          content="고품격 인테리어를 합리적으로 연구하고 적용합니다.
+"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://thecreation.kr" />
+        <meta property="og:title" content="공간제작소 | The Creation" />
+        <meta property="og:image" content="https://thecreation.kr/images/logo/preview.png" />
+        <meta
+          property="og:description"
+          content="고품격 인테리어를 합리적으로 연구하고 적용합니다.
+"
+        />
+        <meta property="og:site_name" content="공간제작소 | The Creation" />
+        <meta property="og:locale" content="ko_KR" />
       </Head>
       <body>
         <Main />
